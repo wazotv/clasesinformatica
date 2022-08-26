14a396bec96c78942c704f2193cf3e07c4a81db7
wazotv <alramirezj@unal.edu.co>
2022-08-26 17:52:52 -0500

    formas de cambiar el tipo de dato

---
+++ formasdecambiardatos.py
@@ -0,0 +1,22 @@
+#formas de cambiar 
+
+#flotante o entero
+#cadena o entero
+
+a="2"
+b=3
+c=int(a)   #funcion int convierte una cadena a entero
+print(b+c)
+
+
+h="3.18"
+f=4
+d=float(h) #la funcion float convierte una cadena a flotante 
+
+print(f+d)
+
+
+y=2
+g=str(y)
+
+print(type(g))  #la funcion str convierte entero o flotante en cadena 
