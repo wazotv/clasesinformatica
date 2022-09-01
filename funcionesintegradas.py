daa806aa1aaf93a485ad62557d6155e462477c40
wazotv <alramirezj@unal.edu.co>
2022-09-01 14:43:15 -0500

    o

---
+++ funcionesintegradas.py
@@ -0,0 +1,13 @@
+
+
+edad=int(input("ingrese su edad")) #interrumpir la ejecucion del codigo, hasta que se almacene un valor.
+
+
+#if edad >=18:
+#    print("eres mayor de edad")
+#else:
+#    print("eres menor de edad")
+print((edad>18 and "es mayor de edad") or "no es mayor de edad")
+
+
+
