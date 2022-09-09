62d7709e82242b39ea78d70681c1b95871588d8c
wazotv <alramirezj@unal.edu.co>
2022-09-09 14:37:57 -0500

    actualizacion

---
+++ 6ciclofor.py
@@ -0,0 +1,35 @@
+
+#cadena="hola--mundo--cruel"
+#
+#or caracter in cadena:
+#   print(caracter)
+#
+#ista=[1,2,30,100,50,-20]
+#or i in lista:
+#   print(i,end="--")
+#
+#upla=(1,2,3,1,2,3,1,2,3)
+#or numero in tupla:
+#   print(numero,end="--")
+#
+#esultados=[1,-1,1,-1,1,-1]
+#
+#or resultado in resultados:
+#   print(resultado,end="--")
+#
+#ango=range(1,100)
+#or v in rango:
+#   print(v,end="--")
+
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    print(i, end="")
+
+for v in range(1,10):
+    print(v,end="")
+
+for b in (1,2,3,4,5,6,7,8,9,10):
+    print(b,end="")
+
+for c in "1,2,3,4,5,6,7,8,9,10":
+    print(c,end="")
---
+++ ciclofor2.py
@@ -0,0 +1,10 @@
+
+
+for i in range(0,21,2):
+    print(i,end="\n")
+
+
+
+for c in range(5,31,):
+    if c%4==0:
+        print(c)
